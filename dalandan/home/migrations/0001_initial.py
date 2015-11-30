13e8298f38bad2d738f12033cb73bf84493e7d69
Peith Vergil <peith.vergil@gmail.com>
2015-11-30 14:01:00 +0800

    Create a migration for the Todo model.

---
+++ dalandan/home/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Todo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(verbose_name='title', max_length=200)),
+                ('status', models.SmallIntegerField(verbose_name='status', choices=[(1, 'Done')])),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+                ('date_updated', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
