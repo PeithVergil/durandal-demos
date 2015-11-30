c5921017d73a31d8d51f44b845f32558b43543a0
Peith Vergil <peith.vergil@gmail.com>
2015-11-30 09:07:26 +0800

    Add the Todo model and serializer.

--- dalandan/home/models.py
+++ dalandan/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Todo(models.Model):
+
+    title = models.CharField(_('title'), max_length=200)
+
+    STATUS_DONE = 1
+
+    STATUS_CHOICES = (
+        (STATUS_DONE, _('Done')),
+    )
+
+    status = models.SmallIntegerField(_('status'), choices=STATUS_CHOICES)
+
+    # Date the todo was created.
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # Date the todo was updated.
+    date_updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def done(self):
+        """
+        Check if the todo is done or not.
+        """
+        return self.status == self.STATUS_DONE
